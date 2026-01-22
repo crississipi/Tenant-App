b6d4999a4c95f53297bd4c4684d0c342f1f1a34f
Crississippi <crismalipico12@gmail.com>
2026-01-23 06:58:23 +0800

    Final Revision.

--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -36,7 +36,7 @@ export async function GET(request: NextRequest) {
         dateIssued: 'desc'
       },
       include: {
-        payments: {
+        Payment: {
           select: {
             amount: true,
             datePaid: true
